28566f90793642b6a8a2b4f7ba9e39c4086f67b2
cdoggett200 <45499165+cdoggett200@users.noreply.github.com>
2018-12-01 15:02:46 -0500

    Step 1 exercise project files

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Pre-Interview Exercise for
+// Craig Doggett
+
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+
+            Console.WriteLine("Enter a comma seperated list of groceries.");
+
+            string stillData = Console.ReadLine();      // get data from user
+            total = computeTotal(stillData);            // compute total of the order
+
+            Console.WriteLine("$" + total);             // output to the screen the total
+        }
+
+        static double computeTotal(string itemsList)
+        {
+            double total = 0;
+            int orangeCount = 0;
+            int appleCount = 0;
+            string groceryItem;
+
+            string[] items = itemsList.Split(',');      // split comma delimited sctring into array
+
+            foreach(var item in items)                  // traverse each items in array
+            {
+                groceryItem = item.Trim();
+                if (groceryItem.Equals("Orange"))       // items is an Orange
+                {
+                    orangeCount++;
+                } else                                  // item is an Apple
+                {
+                    appleCount++;
+                }
+            }
+
+            total = (appleCount * 0.60) + (orangeCount * 0.25);     // compute the total
+            return total; 
+        }
+    }
+}
